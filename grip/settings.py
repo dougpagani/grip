9e9bac4af5876b34c709cccded9c89d3fddc64cc
dougpagani <redstonefreedom@gmail.com>
2018-08-27 04:17:28 -0400

    authentication: do it by macOS keychain, not plaintext

--- grip/settings.py
+++ grip/settings.py
@@ -5,6 +5,8 @@ Do NOT change the values here for risk of accidentally committing them.
 Override them using command-line arguments or with a settings_local.py in
 this directory or in ~/.grip/settings.py instead.
 """
+import subprocess
+import re
 
 
 HOST = 'localhost'
@@ -16,16 +18,21 @@ AUTOREFRESH = True
 QUIET = False
 
 
-# Note: For security concerns, please don't save your GitHub password in your
-# local settings.py. Use a personal access token instead:
-# https://github.com/settings/tokens/new?scopes=
-USERNAME = None
-PASSWORD = None
-
-
 # Custom GitHub API
 API_URL = None
 
 
 # Custom styles
 STYLE_URLS = []
+
+# Grab password from macOS keychain
+def grab_gh_password():
+    invocation = ['security', 'find-internet-password', '-gs', 'github.com']
+    block = subprocess.Popen(invocation, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    lastline = block.stderr.read().strip().decode("utf-8")
+    parsed_password = re.sub('password: "(.*)"', '\\1', lastline)
+    return parsed_password
+
+
+PASSWORD = grab_gh_password()
+USERNAME = 'dougpagani'
